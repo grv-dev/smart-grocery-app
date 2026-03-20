b9bad8b7bb414e035d54d030430df67454bdff2c
grv-dev <gauravjaiswal817@gmail.com>
2026-03-20 09:19:52 +0000

    added unused export

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
  const [theme, setTheme] = useState('light');
 
  const user = {
-   name: 'Neeraj',
+   name: 'Gaurav',
    role: 'Customer'
  };
 
--- src/data/products.js
+++ src/data/products.js
@@ -36,3 +36,5 @@ const products = [
        category: "Dairy",
     }
 ];
+
+export default products;
